af88b46cb995f931bc85a74317d33b8c5925433d
pablodcm179 <castillo.morales.pablo.prog@gmail.com>
2018-12-08 16:51:29 -0600

    password arreglado

--- points/models.py
+++ points/models.py
@@ -1,6 +1,9 @@
 
 from __future__ import unicode_literals
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 class Location(models.Model):
     adress=models.TextField()
@@ -17,11 +20,20 @@ class Team(models.Model):
 
 class User(models.Model):
     correo=models.EmailField(primary_key=True)
-    name=models.CharField(max_length=50)
+    name=models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
     photo=models.URLField(blank=True,null=True)
     description=models.TextField(blank=True,null=True)
     def __str__(self):
         return self.name
+        
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(name=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+	    instance.userprofile.save()
 
 class User_team(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
